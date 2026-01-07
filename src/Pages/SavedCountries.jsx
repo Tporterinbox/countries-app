d37a5d277118da78844dd7339d7f73866199bb8c
Tporterinbox <tporterinbox@gmail.com>
2026-01-07 02:19:48 -0500

    my first commit

---
+++ src/Pages/SavedCountries.jsx
@@ -0,0 +1,16 @@
+
+// Exports the default function component named SavedCountries
+export default function SavedCountries() {
+
+    // The component returns JSX that appears on the screen
+    return (
+
+      // <main> is a semantic HTML element for primary page content
+    // className="Titles" allows for styling this page’s heading in CSS
+      <main className="Titles">
+       {/* Displays the text "My Saved Countries"  on the page with a <h2>heading*/}
+        <h2>My Saved Countries</h2>
+      </main>
+    );
+   
+}
